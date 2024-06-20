98dd93994c53992ecf59f49f0465ad21850ce082
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-06-19 21:31:31 -0300

    Adicionar arquivos de projeto.

---
+++ Estrutura-com-IF/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Estrutura_com_IF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxNumero = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResutado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBoxNumero
+            // 
+            this.txtBoxNumero.Location = new System.Drawing.Point(40, 76);
+            this.txtBoxNumero.Name = "txtBoxNumero";
+            this.txtBoxNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxNumero.TabIndex = 0;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(146, 74);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "Transformar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Conversor para números Positivos";
+            // 
+            // lblResutado
+            // 
+            this.lblResutado.AutoSize = true;
+            this.lblResutado.Location = new System.Drawing.Point(56, 114);
+            this.lblResutado.Name = "lblResutado";
+            this.lblResutado.Size = new System.Drawing.Size(0, 13);
+            this.lblResutado.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 293);
+            this.Controls.Add(this.lblResutado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtBoxNumero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxNumero;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResutado;
+    }
+}
+
---
+++ Estrutura-com-IF/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estrutura_com_IF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtBoxNumero.Text, out double number))
+            {
+                if(number < 0)
+                {
+                    number = Math.Abs(number);
+                }
+
+                lblResutado.Text = $"Resutado: {number}";
+            }
+            else
+            {
+                MessageBox.Show("Por favor, digite um número válido.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
